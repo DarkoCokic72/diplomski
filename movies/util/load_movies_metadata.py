49bad29c398311208cd357cbac6483676f9c0874
DarkoCokic72 <darko.cokic@gmail.com>
2025-08-28 18:09:55 +0200

    added method to get ids of all movies

--- movies/util/load_movies_metadata.py
+++ movies/util/load_movies_metadata.py
@@ -12,6 +12,7 @@ def load_movies_metadata(verbose: bool = True):
 
     return df
 
+def get_movies_ids():
 def load_movies_metadata_small(verbose: bool = True):
     filepath: str = f"../data/movies_metadata_small.csv"
     df = load_file(filepath, sep=",")
