4b136265df8e93d4cf11017fe183fe86811a21a0
DarkoCokic72 <darko.cokic@gmail.com>
2025-09-04 13:20:44 +0200

    exposed api for content based reccomender

--- movies/api/api.py
+++ movies/api/api.py
@@ -1,7 +1,9 @@
 from collaborative.run_collaborative import recommend_items_to_user
 from flask import  Flask, request
+from content_based.run_content_based import ContentBasedRecommender
 
 app = Flask(__name__)
+content_based_recommender = ContentBasedRecommender()
 
 
 @app.route('/')
@@ -11,6 +13,12 @@ def hello_world():
 @app.route('/collab/<int:user_id>', methods=["GET"])
 def recommend_items_by_user_id(user_id: int):
     return recommend_items_to_user(user_id)
+
+@app.route('/content-based/<int:user_id>', methods=["GET"])
+def recommend_to_user_cb(user_id: int):
+    req = request.get_json(force=True)
+    search_string = req["search_string"]
+    return content_based_recommender.recommend_content(search_string)
 
 if __name__ == '__main__':
     app.run(debug=True)
