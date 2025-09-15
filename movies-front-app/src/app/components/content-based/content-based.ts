2fa7ba8491e187fb092f0e3d402d53976317bf08
DarkoCokic72 <darko.cokic@gmail.com>
2025-09-15 11:58:38 +0200

    prototype for content based front page

--- movies-front-app/src/app/components/content-based/content-based.ts
+++ movies-front-app/src/app/components/content-based/content-based.ts
@@ -1,11 +1,83 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+const MODULES = [
+	MatCardModule,
+	MatChipsModule,
+	CommonModule,
+	ReactiveFormsModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatButtonModule,
+	MatProgressSpinnerModule
+]
 
 @Component({
   selector: 'content-based',
-  imports: [],
+  imports: [...MODULES],
   templateUrl: './content-based.html',
   styleUrl: './content-based.css'
 })
 export class ContentBasedComponent {
+  messages: any[] = [
+    {
+      "messageType": "user",
+      "content": "hi"
+    },
+    {
+      "messageType": "system",
+      "content": "hi there"
+    },
+    {
+      "messageType": "user",
+      "content": "lorem"
+    },
+    {
+      "messageType": "system",
+      "content": "ipsum dolor"
+    },
+    {
+      "messageType": "user",
+      "content": "sit amet"
+    },
+    {
+      "messageType": "system",
+      "content": "consectetur faecit"
+    },
+        {
+      "messageType": "user",
+      "content": "hi"
+    },
+    {
+      "messageType": "system",
+      "content": "hi there"
+    },
+    {
+      "messageType": "user",
+      "content": "lorem"
+    },
+    {
+      "messageType": "system",
+      "content": "ipsum dolor"
+    },
+    {
+      "messageType": "user",
+      "content": "sit amet"
+    },
+    {
+      "messageType": "system",
+      "content": "This is a very very very long message that i am about to send. You can't even imagine how long this message will be."
+    },
+  ];
 
+  isUserMessage(message: any){
+    return message.messageType === 'user'
+  }
 }
