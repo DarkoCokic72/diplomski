346bfeafc675531dc5710b7e1d32626d66dc9017
DarkoCokic72 <darko.cokic@gmail.com>
2025-09-17 13:04:14 +0200

    Added Home component

--- movies-front-app/src/app/app.routes.ts
+++ movies-front-app/src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 import { ColaborativeComponent } from './components/colaborative-component/colaborative-component';
 import { ContentBasedComponent } from './components/content-based/content-based';
+import { HomeComponent } from './components/home-component/home-component';
 export const routes: Routes = [
     {
         path: "",
-        component: ColaborativeComponent
+        component: HomeComponent
     },
     {
         path: "colaborative",
         component: ColaborativeComponent
-    }, 
+    },
     {
         path: "content-based",
         component: ContentBasedComponent
--- movies-front-app/src/app/app.ts
+++ movies-front-app/src/app/app.ts
@@ -1,20 +1,25 @@
 import { Component, NgModule, signal } from '@angular/core';
-import { RouterOutlet,  RouterLink } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
 
 
+const MODULES = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule
+]
 
 @Component({
   selector: 'app-root',
   imports: [
+    ...MODULES,
     RouterOutlet,
     RouterLink,
-    MatToolbarModule,
-    MatButtonModule,
-    MatDividerModule, 
-],
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
